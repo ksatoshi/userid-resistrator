701836f4f09b1d12064450810df75d57221ab06b
ksatoshi <ksatoshi@mail.ksatoshi.dev>
2021-12-05 00:46:40 +0900

    jsonデータをログに表示するように変更

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello World"
+
+@app.route('/webhock', methods=['POST'])
+def webhock():
+    print(request.json)
+    return '', 200, {}
 
 if __name__ == '__main__':
     app.run()
