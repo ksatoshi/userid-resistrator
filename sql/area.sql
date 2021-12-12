6349fb1cf7bda3afad351475b6db130054a836a0
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2021-12-12 11:02:39 +0900

    fixed
    DETELE -> DELETE

--- sql/area.sql
+++ sql/area.sql
@@ -2,6 +2,6 @@ CREATE TABLE public.area(
 	id INTEGER NOT NULL UNIQUE,
 	area_name VARCHAR NOT NULL,
 	prefecture_id INTEGER NOT NULL,
-	FOREIGN KEY (prefecture_id) REFERENCES public.prefecture(id) ON DETELE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (prefecture_id) REFERENCES public.prefecture(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY (id)
 );
